81e33fd62b4d6b6bda0429654b64911193a98d7c
Yuutaleo <128760920+Yuutaleo@users.noreply.github.com>
2024-04-05 19:57:31 +0000

    aa

--- app/src/main/java/application/controller/TabuadaController.java
+++ app/src/main/java/application/controller/TabuadaController.java
@@ -1,11 +1,28 @@
 package application.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-
-
-
+@RequestMapping("/tabuada")
 public class TabuadaController{
+    @RequestMapping("/form")
+    public String form(){
+        return "form";
+
+    }
+
+    @RequestMapping("/calcular")
+public String calcular(Model ui, @RequestParam("valor") int valor){
+    int[] resultado = new int[10];
+    for (int num = 1;num <=10; num++ ){
+        resultado[num-1] = num * valor; }
+    ui.addAtribute("tabuada", resultado);
+    return "form";
 
 }
+
+}
+
